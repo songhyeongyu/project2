99ad6d8bd075f3d305131a62dfadb1272c01a4bf
송현규 <lit3207@ajou.ac.kr>
2024-04-28 00:29:15 +0900

    prio 구현중

--- pa2.c
+++ pa2.c
@@ -282,7 +282,7 @@ static struct process *stcf_schedule(void)
 	 */
 	struct process *next = NULL;
 	struct process *tmp = NULL;
-	//preemptive하다, 나보다 더 짧은 순간이 오면 그애를 pick한다.
+	// preemptive하다, 나보다 더 짧은 순간이 오면 그애를 pick한다.
 	if (!current || current->status == PROCESS_BLOCKED)
 	{
 		goto pick_next;
@@ -346,24 +346,23 @@ static struct process *rr_schedule(void)
 
 	/* You may inspect the situation by calling dump_status() at any time */
 	// dump_status();
-	//rr은 preemptive 하다, policy는 process가 실행 중 다른 process가 불리면 정지 현재 프로세스 정지 후 다른프로세스 완료, 다른 프로세스 다시 readyqueue에 넣기
+	// rr은 preemptive 하다, policy는 ,스케줄될때마다(실행될때마다) 다른 프로세스 다시 readyqueue에 넣기
 	if (!current || current->status == PROCESS_BLOCKED)
 	{
 		goto pick_next;
 	}
 
 	if (current->age < current->lifespan)
-	{	
-		list_move_tail(&current->list,&readyqueue);
+	{
+		list_move_tail(&current->list, &readyqueue);
 		goto pick_next;
 	}
-
 
 pick_next:
 
 	if (!list_empty(&readyqueue))
 	{
-		next = list_first_entry(&readyqueue,struct process,list);
+		next = list_first_entry(&readyqueue, struct process, list);
 		list_del_init(&next->list);
 	}
 
@@ -379,13 +378,125 @@ struct scheduler rr_scheduler = {
 /***********************************************************************
  * Priority scheduler
  ***********************************************************************/
+static bool prio_acquire(int resource_id)
+{
+	struct resource *r = resources + resource_id;
+
+	if (!r->owner)
+	{
+		/* This resource is not owned by any one. Take it! */
+		r->owner = current;
+		return true;
+	}
+
+	/* OK, this resource is taken by @r->owner. */
+
+	/* Update the current process state */
+	current->status = PROCESS_BLOCKED;
+
+	/* And append current to waitqueue */
+	list_add_tail(&current->list, &r->waitqueue);
+
+	/**
+	 * And return false to indicate the resource is not available.
+	 * The scheduler framework will soon call schedule() function to
+	 * schedule out current and to pick the next process to run.
+	 */
+	return false;
+}
+
+static void prio_release(int resource_id)
+{
+	struct resource *r = resources + resource_id;
+
+	/* Ensure that the owner process is releasing the resource */
+	assert(r->owner == current);
+
+	/* Un-own this resource */
+	r->owner = NULL;
+
+	/* Let's wake up ONE waiter (if exists) that came first */
+	if (!list_empty(&r->waitqueue))
+	{
+		struct process *waiter = list_first_entry(&r->waitqueue, struct process, list);
+
+		/**
+		 * Ensure the waiter is in the wait status
+		 */
+		assert(waiter->status == PROCESS_BLOCKED);
+
+		/**
+		 * Take out the waiter from the waiting queue. Note we use
+		 * list_del_init() over list_del() to maintain the list head tidy
+		 * (otherwise, the framework will complain on the list head
+		 * when the process exits).
+		 */
+		list_del_init(&waiter->list);
+
+		/* Update the process status */
+		waiter->status = PROCESS_READY;
+
+		/**
+		 * Put the waiter process into ready queue. The framework will
+		 * do the rest.
+		 */
+		list_add_tail(&waiter->list, &readyqueue);
+	}
+}
+static struct process *prio_schedule(void)
+{
+	/**
+	 * Implement your own SJF scheduler here.
+	 */
+	struct process *next = NULL;
+	struct process *tmp = NULL;
+	/* You may inspect the situation by calling dump_status() at any time */
+	// dump_status();
+	if (!current || current->status == PROCESS_BLOCKED)
+	{
+		goto pick_next;
+	}
+
+	if (current->age < current->lifespan)
+	{
+
+		list_for_each_entry(tmp, &readyqueue, list)
+		{
+			// printf("current priority: %d\n",current->prio);
+			if (current->prio < tmp->prio)
+			{
+				next = tmp;
+				next->status = PROCESS_READY;
+				list_add(&current->list, &readyqueue);
+				current->status = PROCESS_BLOCKED;
+				current = next;
+				list_del_init(&next->list);
+				break;
+			}
+		}
+		return current;
+	}
+
+pick_next:
+
+	if (!list_empty(&readyqueue))
+	{
+		next = list_first_entry(&readyqueue, struct process, list);
+		list_del_init(&next->list);
+	}
+
+	return next;
+}
 struct scheduler prio_scheduler = {
 	.name = "Priority",
 	/**
 	 * Implement your own acqure/release function to make the priority
 	 * scheduler correct.
 	 */
+	.acquire = prio_acquire,
+	.release = prio_release,
 	/* Implement your own prio_schedule() and attach it here */
+	.schedule = prio_schedule,
 };
 
 /***********************************************************************
